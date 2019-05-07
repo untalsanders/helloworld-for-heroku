2fd586294c363eca433b159db2359a4cb794e1d6
Sanders Smith Gutiérrez Mena <ing.sanders@gmail.com>
2019-05-06 22:07:35 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const express = require('express')
-const app = express()
-const port = process.env.NODE_PORT || 3000
+let port = process.env.PORT || 3000
+let app = express()
 
 app.get('/', (req, res) => res.send('Hola Mundo desde Heroku'))
 app.listen(port, () => console.log(`App iniciada en http://localhost:${port}`))
