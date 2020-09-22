c875644864e74a8115b85e304f6f54d012642b89
Sanders Gutierrez <ing.sanders@gmail.com>
2020-09-22 06:40:26 -0300

    Support for CSS added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const { join } = require("path");
-const app = require("express")();
+const express = require("express");
 const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
 
 const port = process.env.NODE_PORT || 3000;
+const app = express();
+app.use("/static", express.static(join(process.cwd(), "public")));
+
 let loader = new TwingLoaderFilesystem(join(__dirname, "views"));
 let twing = new TwingEnvironment(loader);
 
