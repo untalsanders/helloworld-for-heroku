a2c91455f5858c7f9ab307151cc128bbb8553084
Sanders Gutierrez <ing.sanders@gmail.com>
2020-09-22 11:50:24 -0300

    HOST environment variable added and the NODE_PORT environment variable was changed to PORT.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const { join } = require("path");
 const express = require("express");
 const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
 
-const port = process.env.NODE_PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
 const app = express();
+
 app.use("/static", express.static(join(process.cwd(), "public")));
 
 let loader = new TwingLoaderFilesystem(join(__dirname, "views"));
@@ -15,4 +17,6 @@ app.get("/", (req, res) => {
     twing.render("index.twig").then((output) => res.end(output));
 });
 
-app.listen(port, () => console.log(`App iniciada en http://localhost:${port}`));
+app.listen(port, host, () =>
+    console.log(`App iniciada en http://${host}:${port}`)
+);
